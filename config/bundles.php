c691c54fab7d56398c0c22008089405fa8a7105b
Sebastien STELTZLEN <sebastien.steltzlen@netapsys.fr>
2019-02-28 15:02:21 +0100

    Suppression des librairies BS non utilisées et optim de la navbar et installation de KNP Paginator

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
